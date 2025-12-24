999c33becbe04fde033e4f9777daaa1c9462fa8e
nknaleena101 <nknaleena101@gmail.com>
2025-12-24 19:50:54 +0530

    gsap from

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,17 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from('#green-box', {
+      x: 250,
+      duration: 2,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut"
+    })
+  },[]);
 
   return (
     <main>
